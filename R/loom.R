21fb4fd1525767f25b73a270ff322aef5fbb80a5
Jimmy Fulp <WilliamJFulp@gmail.com>
2019-04-23 12:48:03 -0700

    adding the saving of source file

--- R/loom.R
+++ R/loom.R
@@ -1,6 +1,7 @@
 #' @export
 loom<-function(){
     activeDoc<-rstudioapi::getActiveDocumentContext()
+    rstudioapi::documentSave(id = activeDoc)
     rmarkdown::render(activeDoc$path, output_format = "all")
 }
 
