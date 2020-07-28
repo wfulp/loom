4e9e1cc261286134cab3b65e49ab39caa60e0439
Ellis Hughes <ehhughes@scharp.org>
2020-07-28 15:01:45 -0700

    resolves #3

--- R/loom.R
+++ R/loom.R
@@ -1,9 +1,17 @@
 #' @export
 loom<-function(){
+
     activeDoc<- rstudioapi::getSourceEditorContext()
     rstudioapi::documentSave(id = activeDoc)
-    outputdocs<-rmarkdown::render(activeDoc$path, output_format = "all")
+    file_full_path <- path.expand(activeDoc$path)
+
+    output <- system(
+            paste0("Rscript -e \"outputdocs<-rmarkdown::render('",file_full_path,"', output_format = 'all');cat(outputdocs, sep='_________')\""),
+            intern = TRUE)
+
+    outputdocs <- strsplit(output[length(output)], split = "_________")[[1]]
 
     for(doc in outputdocs)
         rstudioapi::viewer(doc)
+
 }
