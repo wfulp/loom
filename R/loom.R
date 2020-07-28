4773c836ec211d39cabb724a8fbc33baf28e929a
thebioengineer <ellishughes@live.com>
2020-07-28 16:16:40 -0700

    make noisy hopefully

--- R/loom.R
+++ R/loom.R
@@ -5,11 +5,17 @@ loom<-function(){
     rstudioapi::documentSave(id = activeDoc)
     file_full_path <- path.expand(activeDoc$path)
 
-    output <- system(
-            paste0("Rscript -e \"outputdocs<-rmarkdown::render('",file_full_path,"', output_format = 'all');cat(outputdocs, sep='_________')\""),
-            intern = TRUE)
+    outputfiles_rda <- normalizePath(
+        tempfile(fileext = ".rds"),
+        winslash = "/",mustWork = FALSE)
 
-    outputdocs <- strsplit(output[length(output)], split = "_________")[[1]]
+    system(paste0("Rscript -e \"",
+                  "outputdocs<-rmarkdown::render('",
+                  file_full_path,"', output_format = 'all');",
+                  "saveRDS(outputdocs, '",outputfiles_rda,"')",
+                  "\""))
+
+    outputdocs <- readRDS(outputfiles_rda)
 
     for(doc in outputdocs)
         rstudioapi::viewer(doc)
